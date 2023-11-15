a319a1021c674bfe7111be7797fd4f365fb17a33
Galen Seilis <galen.seilis@seilis.ca>
2023-11-15 14:24:48 -0800

    Update models.py
    COVID Logistic model

--- src/quickprophet/models.py
+++ src/quickprophet/models.py
@@ -1,65 +1,82 @@
-from sklearn.base import BaseEstimator, RegressorMixin
-
-# TODO: By default assume missing dates are zeros
-# TODO: By default assume COVI-19 holidays are the year of 2020
-# TODO: groupby some columns of choice.
-class GroupedAutoProphet(BaseEstimator, RegressorMixin):
-    '''Provides some easy defaults for an end-to-end
-        forecast using different groupings in the data.
-    '''
-
-    def __init__(self, groups, grouped_params=None, t_ind=None:Iterable):
+class BatchCOVIDLogisticProphet:
+    def __init__(self, group_cols, floor=0, cap=7.5 * 60 / 8, datalag=26):
         '''
-        Args:
-            groups (Iterable): Collection of immutable groups.
-            grouped_paras (Dict): Group-specific parameters for the Prophet model.
-            t_ind (Iterable[str]): Temporal indicator variables. Supported values include `weekday`, `month`, `year`. Default is `('weekday',)`.
+        datalag (int): Most recent number of weeks to treat as special dates.
         '''
-        self.groups = groups        
+
+        if not (isinstance(group_cols, list) and len(group_cols) >= 1):
+            raise ValueError(
+                "Must specify a list containing at least one column name to group by."
+            )
+
+        self.group_cols = group_cols
+        self.floor = floor
+        self.cap = cap
+
+        # Prepare special dates for COVID 19
+        self.covid_block = pd.DataFrame(
+            [
+                {
+                    "holiday": "covid19",
+                    "ds": "2020-01-01",
+                    "lower_window": 0,
+                    "ds_upper": "2021-01-01",
+                }
+            ]
+        )
+
+        for t_col in ["ds", "ds_upper"]:
+            self.covid_block[t_col] = pd.to_datetime(self.covid_block[t_col])
+
+        self.covid_block["upper_window"] = (
+            self.covid_block["ds_upper"] - self.covid_block["ds"]
+        ).dt.days
+
+        # Include data lag if specified.
+        if datalag:
+            self.data_lag_block = pd.DataFrame(
+                [
+                    {
+                        "holiday": "data_lag",
+                        "ds": pd.Timestamp.today() - pd.Timedelta(weeks=datalag),
+                        "lower_window": 0,
+                        "ds_upper": pd.Timestamp.today(),
+                    }
+                ]
+            )
+
+            self.data_lag_block["upper_window"] = (
+                self.data_lag_block["ds_upper"] - self.data_lag_block["ds"]
+            ).dt.days
+
+            self.holidays = pd.concat((self.covid_block, self.data_lag_block))
         
-        if grouped_params is not None:
-
-            if set(grouped_params.keys()) - set(groups):
-                raise ValueError('All groups in grouped_params must also be in groups.')
-
-            for group in self.groups:
-                
-                
-                
-            
         else:
+            self.holidays = self.covid_block
 
-            self.grouped_params = {}
-
-            for group in groups:
-                self.grouped_params[group] = {}
-                self.grouped_params[group]['country'] = 'CA' # Sorry, I am Canadian ¯\_(ツ)_/¯
-                # TODO: Set COVID block here
-                self.grouped_params[group] = ('weekday',) # Weekdays almost always explain human behaviour.
-                
-
-
-    def fit(self, X, y=None):
-        '''
-        '''
+    def fit(self, data):
         self.models = {}
+        for group, group_df in data.groupby(self.group_cols):
+            print(f"Trainig Prophet model for {group}.")
 
-        for group, group_df in X.groupby(by=self.groups):
-            ...
+            # Groups are assumed to not be predictors. Make additional predictor columns with the same info if
+            # you need to reuse them.
+            group_df.drop(columns=self.group_cols, inplace=True)
+
+            self.models[group] = Prophet(holidays=self.holidays, growth="logistic")
+
+            # Extra predictors
+            extra_predictors = [
+                col for col in group_df.keys() if col not in ["ds", "y"]
+            ]
+            for predictor in extra_predictors:
+                self.models[group].add_regressor(predictor)
+
+            # Saturation Effects
+            group_df["floor"] = self.floor
+            group_df["cap"] = self.cap
+
+            # Train
+            self.models[group].fit(group_df)
 
         return self
-
-    def predict(self, X):
-        '''
-        Args:
-            X (Iterable[datetime]): Sequence of dates for prediction.
-
-        Returns:
-            pd.DataFrame: Grouped forecasts
-        '''
-
-        forecasts = None
-        for group, group_df in X.groupby(by=self.groups):
-            ...
-        return forecasts
-        
