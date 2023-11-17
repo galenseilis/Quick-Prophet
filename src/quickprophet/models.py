1b91491dca9b74dbe48c77c2ece312e1eca81461
galenseilis <galen.seilis@seilis.ca>
2023-11-16 18:42:56 -0800

    cv method started, but will finish later.

--- src/quickprophet/models.py
+++ src/quickprophet/models.py
@@ -1,8 +1,10 @@
 """Model classes based on Facebook's Prophet."""
 
+import itertools
 
 import pandas as pd
 from prophet import Prophet
+from prophet.diagnostics import cross_validation
 
 class BatchCOVIDLogisticProphet:
     def __init__(self, group_cols, floor=0, cap=7.5 * 60 / 8, datalag=26):
@@ -137,7 +139,31 @@ class BatchCOVIDLogisticProphet:
 
         return forecasts
 
-    def cv(self):
+    def cv(self, param_grid=None):
         """Cross validate."""
-        raise NotImplementedError
+        raise NotImplementedError('Sorry, not ready yet.')
+
+        # TODO: Need to do the following for each group.
+        if param_grid is None:
+            param_grid = {
+                        'changepoint_prior_scale': [0.001, 0.01, 0.1, 0.5],
+                        'seasonality_prior_scale': [0.01, 0.1, 1.0, 10.0],
+                        }
+
+        # Generate all combinations of parameters
+        all_params = [dict(zip(param_grid.keys(), v)) for v in itertools.product(*param_grid.values())]
+        rmses = []  # Store the RMSEs for each params here
+
+        # Use cross validation to evaluate all parameters
+        for params in all_params:
+            m = Prophet(**params).fit(df)  # Fit model with given params
+            df_cv = cross_validation(m, cutoffs=cutoffs, horizon='30 days', parallel="processes")
+            df_p = performance_metrics(df_cv, rolling_window=1)
+            rmses.append(df_p['rmse'].values[0])
+
+        # Find the best parameters
+        tuning_results = pd.DataFrame(all_params)
+        tuning_results['rmse'] = rmses
+        print(tuning_results)
+
         return self
